fd378ee7dd24aaff9df89e4013021937266f78b6
stzkdigitalmedia <Krunal.vegda02@gmail.com>
2026-03-24 11:34:55 +0530

    cors everywhere

--- src/app.js
+++ src/app.js
@@ -38,13 +38,7 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin: true, // Allow all origins for testing
     credentials: true,
   })
 );
