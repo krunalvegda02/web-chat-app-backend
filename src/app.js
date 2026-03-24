ec7cde3a1f7496452f120cef3f506d23cf42d06c
stzkdigitalmedia <Krunal.vegda02@gmail.com>
2026-03-24 19:13:11 +0530

    rrrpay cors

--- src/app.js
+++ src/app.js
@@ -39,7 +39,7 @@ const allowedOrigins = [
   'https://212.90.120.17/', // HTTPS with trailing slash
   'https://212.90.120.17',   // HTTPS without trailing slash
 
-  "https://rrrpay.co/",
+  "https://rrrpay.co",
 ].filter(Boolean); // Remove undefined values
 
 app.use(
